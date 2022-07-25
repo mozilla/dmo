b848d3037583d52062749ac1e2891db8b787f0dd
Linh Nguyen <linhnguyen@mozilla.com>
2022-07-24 23:33:15 -0400

    Re-add Protocol

---
+++ src/styles/protocol/css/includes/tokens/dist/content/content.common.js
@@ -0,0 +1,13 @@
+module.exports = {
+  contentXs: "304px",
+  contentSm: "432px",
+  contentMd: "688px",
+  contentLg: "928px",
+  contentXl: "1152px",
+  contentMax: "1440px",
+  screenXs: "320px",
+  screenSm: "480px",
+  screenMd: "768px",
+  screenLg: "1024px",
+  screenXl: "1312px",
+};
