5e93f33a8f3357d6252c0533d80d72b1f27c0192
Linh Nguyen <linhnguyen@mozilla.com>
2022-07-24 23:04:20 -0400

    Use Mozilla Protocol for styling

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import netlify from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({
+		scss: {
+			prependData: `@import './src/styles/protocol/protocol.scss';`
+		}
+	}),
 	kit: {
-		adapter: adapter()
+		adapter: netlify()
+	},
+	onwarn: (warning, handler) => {
+		const { code } = warning;
+		if (code === 'css-unused-selector') return;
+		handler(warning);
 	}
 };
 
