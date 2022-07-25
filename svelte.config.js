42f8f276e8adfd1b0531be42fa124c3bb0cdc14b
Linh Nguyen <linhnguyen@mozilla.com>
2022-07-24 23:24:59 -0400

    Restart skeleton project to use the latest SvelteKit update

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,9 @@
-import netlify from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		scss: {
-			prependData: `@import './src/styles/protocol/protocol.scss';`
-		}
-	}),
 	kit: {
-		adapter: netlify()
-	},
-	onwarn: (warning, handler) => {
-		const { code } = warning;
-		if (code === 'css-unused-selector') return;
-		handler(warning);
+		adapter: adapter()
 	}
 };
 
