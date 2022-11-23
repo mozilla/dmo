77ecfe54922f2b00d255e3668f2ed67324aaf024
Linh Nguyen <linhnguyen@mozilla.com>
2022-11-23 12:54:13 -0500

    Update product page, and minor frontend fixes (#10)
    * Update product page, and minor frontend fixes
    
    * Remove dashboard image because we don't need it anymore
    
    * Address review comments

--- src/lib/data/products.js
+++ src/lib/data/products.js
@@ -8,7 +8,7 @@ export const kpis = [
 		url: ' https://mozilla.cloud.looker.com/dashboards/101'
 	},
 	{
-		name: 'Numbers That Matter',
+		name: 'Firefox Desktop New Users',
 		url: 'https://mozilla.cloud.looker.com/dashboards/66'
 	},
 	{
@@ -23,63 +23,51 @@ export const products = [
 		app_id: 'firefox_desktop',
 		dashboards: [
 			{
-				name: 'Looker dashboards',
-				description: 'Looker is the primary tool for analyzing data at Mozilla.',
-				links: [
-					{
-						name: 'Looker folder',
-						description:
-							'Firefox desktop dashboards and explores: KPI, Search, User Journey, Translations, etc.',
-						url: 'https://mozilla.cloud.looker.com/folders/747'
-					}
-				]
+				name: 'Looker Folder',
+				description:
+					'Firefox desktop dashboards and explores: KPI, Search, User Journey, Translations, etc.',
+				url: 'https://mozilla.cloud.looker.com/folders/747'
 			},
 			{
-				name: 'More',
-				description: "Dashboards that we haven't migrated to Looker yet",
-				links: [
-					{
-						name: 'Default Browser Setting By Platform',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/default-browser-setting-by-platform',
-						description: 'Default browser setting in Desktop, iOS, Android'
-					},
-					{
-						name: 'New profiles and retention per country',
-						url: 'https://sql.telemetry.mozilla.org/queries/69256/source#174699',
-						description:
-							"New profiles and retention per country ('US', 'GB', 'CA', 'FR', 'DE','CN','IN','BR')"
-					},
-					{
-						name: 'New Profile Activation Timeseries 28 Day Basis',
-						url: 'https://sql.telemetry.mozilla.org/queries/74181/source#187560',
-						description: 'The number of users meeting criteria per 28D period, etc.'
-					},
-					{
-						name: 'Regular VS core users per segments',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/regular-vs-core-users-per-segments',
-						description: 'Cohorts of users by days of engagement. Core = 21+ days out of 28.'
-					},
-					{
-						name: 'Regular / Core Users',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/regular-core-users',
-						description: 'An Alternative Metric to Consider: Weekly (or Monthly) Usage Days'
-					},
-					{
-						name: 'Default Agent Dashboard',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/default-agent-dashboard',
-						description: 'Users moving from FF to Edgium VS users moving from Edgium to FF on Win10'
-					},
-					{
-						name: 'Daily users - shown vs recovered',
-						url: 'https://sql.telemetry.mozilla.org/queries/76606/source#191008',
-						description: 'WDBA notifications - % based'
-					},
-					{
-						name: 'SAP Usage Share - Weekly',
-						url: 'https://sql.telemetry.mozilla.org/queries/56499#147865',
-						description: 'Recorded in release channel'
-					}
-				]
+				name: 'Default Browser Setting By Platform',
+				description: 'Default browser setting in Desktop, iOS, Android',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/default-browser-setting-by-platform'
+			},
+			{
+				name: 'New Profiles and Retention per Country',
+				description:
+					"New profiles and retention per country ('US', 'GB', 'CA', 'FR', 'DE','CN','IN','BR')",
+				url: 'https://sql.telemetry.mozilla.org/queries/69256/source#174699'
+			},
+			{
+				name: 'New Profile Activation Timeseries 28 Day Basis',
+				description: 'The number of users meeting criteria per 28D period, etc.',
+				url: 'https://sql.telemetry.mozilla.org/queries/74181/source#187560'
+			},
+			{
+				name: 'Regular VS Core Users per Segments',
+				description: 'Cohorts of users by days of engagement. Core = 21+ days out of 28.',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/regular-vs-core-users-per-segments'
+			},
+			{
+				name: 'Regular / Core Users',
+				description: 'An Alternative Metric to Consider: Weekly (or Monthly) Usage Days',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/regular-core-users'
+			},
+			{
+				name: 'Default Agent Dashboard',
+				description: 'Users moving from FF to Edgium VS users moving from Edgium to FF on Win10',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/default-agent-dashboard'
+			},
+			{
+				name: 'Daily Users - Shown vs Recovered',
+				description: 'WDBA notifications - % based',
+				url: 'https://sql.telemetry.mozilla.org/queries/76606/source#191008'
+			},
+			{
+				name: 'SAP Usage Share - Weekly',
+				description: 'Recorded in release channel',
+				url: 'https://sql.telemetry.mozilla.org/queries/56499#147865'
 			}
 		]
 	},
@@ -88,31 +76,19 @@ export const products = [
 		app_id: 'firefox_mobile',
 		dashboards: [
 			{
-				name: 'Looker dashboards',
-				description: 'Looker is the primary tool for analyzing data at Mozilla.',
-				links: [
-					{
-						name: 'Looker folder',
-						description: 'Android, iOS, Focus Android, Focus iOS, etc.',
-						url: 'https://mozilla.cloud.looker.com/folders/748'
-					}
-				]
+				name: 'Looker Folder',
+				description: 'Android, iOS, Focus Android, Focus iOS, etc.',
+				url: 'https://mozilla.cloud.looker.com/folders/748'
 			},
 			{
-				name: 'More',
-				description: "Dashboards that we haven't migrated to Looker yet",
-				links: [
-					{
-						name: 'Mobile Product - Search Volume',
-						description: 'Total Volume Search - Fenix, Firefox iOS, Focus, Legacy',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/search---mobile-products#156933'
-					},
-					{
-						name: 'Mobile Search Accuracy',
-						description: 'Google Revenue Efficiency',
-						url: 'https://sql.telemetry.mozilla.org/dashboard/mobile-search-accuracy'
-					}
-				]
+				name: 'Mobile Product - Search Volume',
+				description: 'Total Volume Search - Fenix, Firefox iOS, Focus, Legacy',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/search---mobile-products#156933'
+			},
+			{
+				name: 'Mobile Search Accuracy',
+				description: 'Google Revenue Efficiency',
+				url: 'https://sql.telemetry.mozilla.org/dashboard/mobile-search-accuracy'
 			}
 		]
 	},
@@ -124,7 +100,7 @@ export const products = [
 			{
 				name: 'Looker dashboards',
 				description: 'Looker is the primary tool for analyzing data at Mozilla.',
-				links: [
+				url: [
 					{
 						name: 'Looker folder',
 						description: 'Pocket Looker folder',
@@ -140,15 +116,9 @@ export const products = [
 		url: 'https://mozilla.cloud.looker.com/folders/375',
 		dashboards: [
 			{
-				name: 'Looker dashboards',
-				description: 'Looker is the primary tool for analyzing data at Mozilla.',
-				links: [
-					{
-						name: 'Looker folder',
-						description: 'VPN Looker folder',
-						url: 'https://mozilla.cloud.looker.com/folders/375'
-					}
-				]
+				name: 'Looker Folder',
+				description: 'VPN Looker folder',
+				url: 'https://mozilla.cloud.looker.com/folders/375'
 			}
 		]
 	}
--- src/routes/products/[product].svelte
+++ src/routes/products/[product].svelte
@@ -14,29 +14,18 @@
 	</div>
 </section>
 
-{#each product['dashboards'] as section}
-	<div class="mzp-l-content mzp-l-card-quarter">
-		<h3>{section.name}</h3>
-		<p>{section.description}</p>
-		{#each section.links as dashboard}
-			<section class="mzp-c-card">
-				<a class="mzp-c-card-block-link" href={dashboard.url}>
-					<div class="mzp-c-card-media-wrapper">
-						<img class="mzp-c-card-image" src="/dashboard.jpg" alt="" />
-					</div>
-					<div class="mzp-c-card-content">
-						<h6>{dashboard.name}</h6>
-						<p>{dashboard.description}</p>
-					</div>
-				</a>
-			</section>
-		{/each}
-	</div>{/each}
+<div class="mzp-l-content grid">
+	{#each product['dashboards'] as dashboard}
+		<a class="dashboard mzp-c-emphasis-box" href={dashboard.url}>
+			<h4>{dashboard.name}</h4>
+			<p>{dashboard.description}</p>
+		</a>
+	{/each}
+</div>
 
 <style lang="scss">
-	@import './src/styles/protocol/css/components/_card.scss';
-	@import './src/styles/protocol/css/templates/_card-layout.scss';
 	@import './src/styles/protocol/css/components/_call-out.scss';
+	@import './src/styles/protocol/css/components/_emphasis-box.scss';
 
 	.mzp-c-card-content {
 		p {
@@ -46,5 +35,21 @@
 	}
 	.mzp-c-card-media-wrapper {
 		box-shadow: 0 0 0 2px $color-marketing-gray-20;
+	}
+	.dashboard {
+		display: block;
+		cursor: pointer;
+		text-decoration: none;
+		color: $color-dark-gray-60;
+		&:hover {
+			h4 {
+				color: $color-blue-50;
+			}
+			box-shadow: 0 8px 12px 1px #1d11332a, 0 3px 16px 2px #09204d1f, 0 5px 10px -3px #1d11331f;
+		}
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
 	}
 </style>
