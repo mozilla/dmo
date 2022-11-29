5a2e5e7a40a395065c8b542ac0e7bed0280808c4
Linh Nguyen <linhnguyen@mozilla.com>
2022-11-29 12:33:59 -0500

    Use meta.env for env vars

--- src/auth_config.js
+++ src/auth_config.js
@@ -1,4 +1,4 @@
 export const config = {
-	domain: process.env.VITE_PUBLIC_AUTH0_DOMAIN || 'auth.mozilla.auth0.com',
-	clientId: process.env.VITE_PUBLIC_AUTH0_CLIENT_ID || ''
+	domain: import.meta.env.VITE_PUBLIC_AUTH0_DOMAIN || 'auth.mozilla.auth0.com',
+	clientId: import.meta.env.VITE_PUBLIC_AUTH0_CLIENT_ID || ''
 };
